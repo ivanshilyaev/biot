9c614afc86242decea780acdcc3e7a7bafa53183
Ivan Shilyaev <shilyaev1433@gmail.com>
2022-04-20 18:51:01 +0300

    Set up remote WebSerial monitor

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -6,17 +6,21 @@
 #include <WebSerial.h>
 
 int LED = D1;
-WiFiServer server(80);
-AsyncWebServer server(81);
+AsyncWebServer server(80);
 
 /* Message callback of WebSerial */
 void recieveMessage(uint8_t *data, size_t len){
-  WebSerial.println("Received Data...");
   String d = "";
   for(int i=0; i < len; i++){
     d += char(data[i]);
   }
   WebSerial.println(d);
+  if (d == "on") {
+    digitalWrite(LED, HIGH);
+  }
+  if (d == "off") {
+    digitalWrite(LED, LOW);
+  }
 }
 
 void setup() {
@@ -24,7 +28,6 @@ void setup() {
   pinMode(LED, OUTPUT);
   digitalWrite(LED, LOW);
 
-  // Set up WebSerial
   WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   while (WiFi.status() != WL_CONNECTED) {
@@ -36,49 +39,10 @@ void setup() {
   Serial.println("Connected");
   Serial.println(WiFi.localIP());
 
+  WebSerial.begin(&server);
+  WebSerial.msgCallback(recieveMessage);
   server.begin();
-  Serial.println("Server started");
 }
 
 void loop() {
-  WiFiClient client = server.available();
-  if (!client) { return; }
-
-  Serial.println("Waiting for a client to connect...");
-  while (!client.available()) { delay(1); }
-  
-  String request = client.readStringUntil('\r');
-  Serial.println(request);
-  client.flush();
-
-  int value = LOW;
-  if (request.indexOf("/LED=ON") != -1) {
-    digitalWrite(LED, HIGH);
-    value = HIGH;
-  }
-  if (request.indexOf("/LED=OFF") != -1) {
-    digitalWrite(LED, LOW);
-    value = LOW;
-  }
-
-  
-  // standalone web server with an ESP8266
-  client.println("HTTP/1.1 200 OK");
-  client.println("Content-Type: text/html");
-  client.println("");
-  client.println("<!DOCTYPE HTML>");
-  client.println("<html>");
-
-  client.print("LED: ");
-
-  if (value == HIGH) { client.print("ON"); }
-  else { client.print("OFF"); }
-  client.println("<br><br>");
-  client.println("<a href=\"/LED=ON\"\"><button>ON</button></a>");
-  client.println("<a href=\"/LED=OFF\"\"><button>OFF</button></a><br />");
-  client.println("</html>");
-
-  delay(1);
-  Serial.println("Client disonnected");
-  Serial.println("");
 }
