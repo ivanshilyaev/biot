241b37e14ceb2f5652a961b7d370a41655453949
Ivan Shilyaev <shilyaev1433@gmail.com>
2022-04-26 21:33:34 +0300

    Try to use C code in Java

--- client/src/main/java/com/ivanshilyaev/Runner.java
+++ client/src/main/java/com/ivanshilyaev/Runner.java
@@ -1,14 +1,23 @@
 package com.ivanshilyaev;
 
-import java.io.IOException;
+import com.sun.jna.Library;
+import com.sun.jna.Native;
 
 public class Runner {
 
+    public interface exampleDylib extends Library {
+        exampleDylib INSTANCE = Native.load("library", exampleDylib.class);
+
+        void bash_f();
+    }
+
     public static void main(String[] args) {
-        try {
-            new ClientService().turnOff();
-        } catch (IOException | InterruptedException e) {
-            System.out.println(e.getMessage());
-        }
+        exampleDylib.INSTANCE.bash_f();
+
+//        try {
+//            new ClientService().turnOn();
+//        } catch (IOException | InterruptedException e) {
+//            System.out.println(e.getMessage());
+//        }
     }
 }
