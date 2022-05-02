57e780cf235cbf437a440492f0ff8148eee456a4
Ivan Shilyaev <shilyaev1433@gmail.com>
2022-05-02 10:36:25 +0200

    Rewrite start command (77 standard)

--- client/src/main/java/Runner.java
+++ client/src/main/java/Runner.java
@@ -1,28 +1,9 @@
-import utils.HexEncoder;
+import encryption.AuthenticatedEncryptionService;
 
 public class Runner {
 
-    static {
-        System.loadLibrary("LibraryNative");
-    }
-
     public static void main(String[] args) {
-        String input =
-                "B194BAC80A08F53B366D008E584A5DE48504FA9D1BB6C7AC252E72C202FDCE0D" +
-                "5BE3D61217B96181FE6786AD716B890B5CB0C0FF33C356B835C405AED8E07F99" +
-                "E12BDC1AE28257EC703FCCF095EE8DF1C1AB76389FE678CAF7C6F860D5BB9C4F" +
-                "F33C657B637C306ADD4EA7799EB23D313E98B56E27D3BCCF591E181F4C5AB793" +
-                "E9DEE72C8F0C0FA62DDB49F46F73964706075316ED247A3739CBA38303A98BF6" +
-                "92BD9B1CE5D141015445FBC95E4D0EF2682080AA227D642F2687F934904055111";
-        byte[] array = HexEncoder.decode(input);
-        byte[] result = LibraryNative.bash_f(array);
-        String output = HexEncoder.encode(result);
-
-        for (int i = 0; i < 384; i+=16) {
-            System.out.print(output.substring(i, i+16) + " ");
-            if ((i+16) % 64 == 0) {
-                System.out.println();
-            }
-        }
+        AuthenticatedEncryptionService service = new AuthenticatedEncryptionService();
+        service.start();
     }
 }
--- client/src/main/java/encryption/AuthenticatedEncryptionService.java
+++ client/src/main/java/encryption/AuthenticatedEncryptionService.java
@@ -2,11 +2,10 @@ package encryption;
 
 import utils.HexEncoder;
 
-import java.util.BitSet;
-
 public class AuthenticatedEncryptionService {
 
     private static final int N = 1536;
+    private static final int nBytes = 192;
 
     // уровень стойкости
     private static final int l = 128;
@@ -15,12 +14,12 @@ public class AuthenticatedEncryptionService {
     private static final int d = 1;
 
     // анонс, 16 base
-    private static final BitSet A = BitSet.valueOf(HexEncoder.decode("11111111"));
+    private static final byte[] A = HexEncoder.decode("1111111111111111");
 
     // ключ, 16 base
-    private static final BitSet K = BitSet.valueOf(HexEncoder.decode("12345678123456781234567812345678"));
+    private static final byte[] K = HexEncoder.decode("12345678123456781234567812345678");
 
-    private static BitSet S = new BitSet(N);
+    private static byte[] S = new byte[nBytes];
 
     private static int r;
 
@@ -29,26 +28,28 @@ public class AuthenticatedEncryptionService {
     public void start() {
         // 1.
         if (K != null) {
-            r = (int) (1536 - l - 0.5 * d * l);
+            r = (int) (N - l - 0.5 * d * l);
         }
         // 2.
         else {
-            r = 1536 - 2 * d * l;
+            r = N - 2 * d * l;
         }
         // 3. pos <- 8 + |ann| + |key|
-        pos = 8 + A.length() + 4 * K.length();
+        pos = 8 * (1 + A.length + K.length);
         // 4. s[0..pos) <- <|ann|/2 + |key|/32>_8 || ann || key
-        int U = (int) ((A.length() / 2 + K.length() / 32) % (Math.pow(2, 8)));
-        String temp = String.format("%8s", Integer.toBinaryString(U)).replaceAll(" ", "0");
-        for (int i = 0; i < temp.length(); ++i) {
-            S.set(i, temp.charAt(i) == '1');
-        }
-        for (int i = 0; i < A.length(); ++i) {
-            S.set(8 + i, A.get(i));
-        }
-        for (int i = 0; i < K.length(); ++i) {
-            S.set(8 + A.length() + i, K.get(i));
-        }
+        int U = (int) ((8 * A.length / 2 + 8 * K.length / 32) % (Math.pow(2, 8)));
+        S[0] = (byte) (U - 128);
+        System.arraycopy(A, 0, S, 1, A.length);
+        System.arraycopy(K, 0, S, 1 + A.length, K.length);
         // 5.
+        for (int i = pos / 8; i < 1472 / 8; ++i) {
+            S[i] = 0;
+        }
+        // 6.
+        U = (int) ((l / 4 + d) % (Math.pow(2, 64)));
+        for (int i = 1472 / 8; i < nBytes - 1; ++i) {
+            S[i] = 0;
+        }
+        S[nBytes - 1] = (byte) (U - 128);
     }
 }
